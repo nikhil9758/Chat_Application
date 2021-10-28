5a4340df4a22ff5e5e36dd05c98e2f6c41180af2
nikhil <nikhilgupta9758@gmail.com>
2021-10-29 00:11:20 +0530

    commit

--- JS/client.js
+++ JS/client.js
@@ -3,21 +3,39 @@ const socket = io('http://localhost:8000');
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector('.container')
+let messageid=0;
 
 const append = (message, position)=>{
     const messageElement = document.createElement('div');
+    var btn= document.createElement("BUTTON");
+    messageElement.id=socket.id+messageid;
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position)
     messageContainer.append(messageElement);
+
+    var btnRemove = document.createElement("INPUT");
+    btnRemove.value = "Remove";
+    btnRemove.type = "button";
+    btnRemove.classList.add('button');
+    btnRemove.onclick = function (event) {
+        console.log(event);
+        console.log(event.target.parentElement.id);
+        socket.emit('delete-msg',event.target.parentElement.id);
+        messageElement.remove(this.parentNode);
+    };
+
+    messageElement.appendChild(btnRemove);
+
  }
 
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
+    messageid= messageid+1;
     const message = messageInput.value;
     append(`You: ${message}`, 'right');
-    socket.emit('send', message);
+    socket.emit('send', {msg:message,id:messageid});
     messageInput.value = '';
 })
 
@@ -32,6 +50,6 @@ socket.on('receive', data=>{
     append(`${data.name }: ${data.message}`, 'left')
 })
 
-// socket.on('left', name=>{
-//     append(`${name } left the chat`, 'left');
-// })
+socket.on('left', name=>{
+    append(`${name } left the chat`, 'left');
+})
